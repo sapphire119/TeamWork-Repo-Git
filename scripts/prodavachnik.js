fa55e24455349b20100055fcfc369f4364301836
kvadrat4o <vasilevdimitur@gmail.com>
2017-10-17 21:17:01 +0300

    API keys updated and functions for login user updated. Section for Advertisement added

--- scripts/prodavachnik.js
+++ scripts/prodavachnik.js
@@ -7,11 +7,17 @@ function startApp() {
     $("#linkHome").click(showHomeView);
     $("#linkLogin").click(showLoginView);
     $("#linkRegister").click(showRegisterView);
+    $("#linkListAds").click(listAdverts);
     $("#linkLogout").click(logoutUser);
 
     // Bind the form submit buttons
     $("#buttonLoginUser").click(loginUser);
     $("#buttonRegisterUser").click(registerUser);
+
+    const kinveyBaseUrl = "https://mock.api.com/";
+    const kinveyAppKey = "kid_rk";
+    const kinveyAppSecret = "736804a668";
+
 
     function showView(viewName) {
         // Hide all views and show the selected view only
@@ -29,11 +35,13 @@ function startApp() {
             // No logged in user
             $("#linkLogin").show();
             $("#linkRegister").show();
+            $("#linkListAds").hide();
             $("#linkLogout").hide();
         } else {
             // We have logged in user
             $("#linkLogin").hide();
             $("#linkRegister").hide();
+            $("#linkListAds").show();
             $("#linkLogout").show();
         }
     }
@@ -50,9 +58,9 @@ function startApp() {
 
     // user/login
     function loginUser() {
-        const kinveyLoginUrl = "https://mock.backend.com/user/kid_rk/login";
+        const kinveyLoginUrl = kinveyBaseUrl + "user/" + kinveyAppKey + "/login";
         const kinveyAuthHeaders = {
-            'Authorization': "Basic " + btoa("kid_rk:736804a668"),
+            'Authorization': "Basic " + btoa(kinveyAppKey + ":" + kinveyAppSecret),
         };
         let userData = {
             username: $('#formLogin input[name=username]').val(),
@@ -70,7 +78,7 @@ function startApp() {
         function loginSuccess(userInfo) {
             saveAuthInSession(userInfo);
             showHideMenuLinks();
-            showHomeView();
+            listAdverts();
         }
     }
 
@@ -83,9 +91,9 @@ function startApp() {
 
     // user/register
     function registerUser() {
-        const kinveyRegisterUrl = "https://mock.backend.com/user/kid_rk/";
+        const kinveyRegisterUrl = kinveyBaseUrl + "user/" + kinveyAppKey + "/";
         const kinveyAuthHeaders = {
-            'Authorization': "Basic " + btoa("kid_rk:736804a668"),
+            'Authorization': "Basic " + btoa(kinveyAppKey + ":" + kinveyAppSecret),
         };
 
         let userData = {
@@ -105,7 +113,7 @@ function startApp() {
             console.log(userInfo);
             saveAuthInSession(userInfo);
             showHideMenuLinks();
-            showHomeView();
+            listAdverts();
         }
     }
 
@@ -115,6 +123,48 @@ function startApp() {
         $('#loggedInUser').text("");
         showHideMenuLinks();
         showHomeView();
+    }
+
+    // advertisement/all
+    function listAdverts() {
+        $('#ads').empty();
+        showView('viewAds');
+
+        const kinveyAdvertsUrl = kinveyBaseUrl + "appdata/" + kinveyAppKey + "/adverts";
+        const kinveyAuthHeaders = {
+            'Authorization': "Kinvey " + sessionStorage.getItem('authToken'),
+        };
+        $.ajax({
+            method: "GET",
+            url: kinveyAdvertsUrl,
+            headers: kinveyAuthHeaders,
+            success: loadAdvertsSuccess
+        });
+
+        function loadAdvertsSuccess(adverts) {
+            if (adverts.length === 0) {
+                $('#ads').text('No advertisements available.');
+            } else {
+                let advertsTable = $('<table>')
+                    .append($('<tr>').append(
+                        '<th>Title</th>',
+                        '<th>Publisher</th>',
+                        '<th>Date Published</th>',
+                        '<th>Price</th>')
+                    );
+
+                for (let advert of adverts) {
+                    advertsTable.append($('<tr>').append(
+                        $('<td>').text(advert.title),
+                        $('<td>').text(advert.publisher),
+                        $('<td>').text(advert.datePublished),
+                        $('<td>').text(advert.price)
+                    ));
+                }
+
+                $('#ads').append(advertsTable);
+            }
+        }
     }
 }
 
